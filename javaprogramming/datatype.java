7d6dbcc0c2bfe3b25a31b8b1e78e6833b753d6ee
ashwini p <Ashwinipashu13@gmail.com>
2025-05-30 20:55:02 +0530

    Add files via upload

---
+++ javaprogramming/datatype.java
@@ -0,0 +1,24 @@
+public class datatype {
+    public static void main(String[] args) {
+      int myNum = 5;               // integer (whole number)
+    float myFloatNum = 5.99f;    // floating point number
+    char myLetter = 'D';         // character
+    boolean myBool = true;       // boolean
+    String myText = "Hello";     // String    
+      System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText); 
+
+    int items = 50;
+    float costPerItem = 9.99f;
+    float totalCost = items * costPerItem;
+    char currency = '$';
+
+    // Print variables
+    System.out.println("Number of items: " + items);
+    System.out.println("Cost per item: " + costPerItem + currency);
+    System.out.println("Total cost = " + totalCost + currency);
+    }
+}
